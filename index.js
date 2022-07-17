cc19b91c5014abf83b221c5306bbbc9890d5708c
praveenaThota97 <thotapraveena97@gmail.com>
2022-07-17 12:43:34 +0530

    My code commit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
+
+
 console.log("Hello Node");
 console.log("Appuveena");
+app.listen(4000,()=>{
+    console.log("server Started");
+})
+app.get('/',(req,res)=>{
+    res.send("Hello Node! I am Appuveena");
+});
+
+app.get('/formStudentData',(req,res)=>{
+    let studentId = req.query.studentid;
+    res.send(studentId);
+    console.log(req);
+});
+
+
